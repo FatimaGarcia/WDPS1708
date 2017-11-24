ed511b285f8247c40eebf2749a37d64059f24b19
FatimaGarcia <f.garciac@alumnos.upm.es>
2017-11-24 02:44:43 -0800

    Bash script-2

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
+
 if [ $# -ne 2 ]; then
 	echo $0: Usage: \<Warc-key\> \<Input-File-Path\> 
 	exit 1
 fi
-
-ATT=${1:-"WARC-Record-ID"}
-INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
 
 PYSPARK_PYTHON=$(readlink -f $(which python)) ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --master yarn A1_WDPS1708.py $1 $2
